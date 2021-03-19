36f91e776c48750ed24673dda0fe78d6ddf9fbaa
Marcelo Barreto <marcelo_barreto_s@hotmail.com>
2021-03-19 20:51:08 -0300

    Final Commit

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getUsers, createUser, getUser, deleteUser, patchUser } from '../controllers/users.js';
+
+const router = express.Router();
+
+router.get('/', getUsers);
+router.post('/', createUser);
+router.get('/:id', getUser);
+router.delete('/:id', deleteUser);
+router.patch('/:id', patchUser);
+
+export default router;
